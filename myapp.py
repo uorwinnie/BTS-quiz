7e216199dc42131db1cbd6368556cad25338ea54
whThant <wuthmonthant005@gmail.com>
2024-01-15 12:31:50 +0630

    UPDATE

--- myapp.py
+++ myapp.py
@@ -9,17 +9,21 @@ def greet():
 @app.route("/result/<int:mark>")
 def result(mark):
     if 101> mark >=80:
-        return f"You got grade (A) with the mark of {mark}"
+        if mark==100:
+            return f"You got grade (A) with full mark!!. Great job."
+        else:
+            return f"You got grade (A) with the mark of {mark}"
     elif 80 > mark >=60:
         return f"You got grade (B) with the mark of {mark}"
     elif 60> mark  >=40:
         return f"You got grade (C) with the mark of {mark}"
-    elif 40> mark:
+    elif 40> mark >=0:
         if mark ==0:
-            return f"Try much harder next time. You got D"
+            return f"ZERO!! Really? Try much harder next time. You got D"
         elif mark == 39:
             return f"Failed with just one mark. Gook luck next time. You got D"
-        return f"You got grade (D) with the mark of {mark}"
+        else:
+            return f"You got grade (D) with the mark of {mark}"
     else:
         return f"This isn't funny"
 
